d4eb45b71231d3f39704f20a9b8f2a040e55b7fd
Chiamaka  Anyanwu <93395990+grace2709@users.noreply.github.com>
2023-08-08 14:40:47 +0100

    Add files via upload
    Python program to check if a number is even

---
+++ checkEven.py
@@ -0,0 +1,8 @@
+#program to check if a number is even or not
+
+number = float(input("Enter a number"))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
